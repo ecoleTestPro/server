46935a94b46e16f0b1aa37e27974f60885c64f6c
eraste-dev <keraste38@gmail.com>
2025-05-31 09:27:32 +0000

    feat: Add testimonial component and card for displaying user feedback

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -2,6 +2,7 @@ import AboutUsCard from '@/components/aboutUs/aboutUsCard';
 import Bestseller from '@/components/courses/bestseller';
 import FeaturesSection from '@/components/hero/featuresSection';
 import HeroHomePage from '@/components/hero/HeroHomePage';
+import Testimonial from '@/components/testimonial/testimonial';
 import DefaultLayout from '@/layouts/public/front.layout';
 import { type SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
