3d4c557cf033b3c6ba5065e24fcfe4190847adb8
eraste-dev <keraste38@gmail.com>
2025-07-01 20:58:17 +0000

    maj

--- resources/js/components/contactUs/ContactUs.tsx
+++ resources/js/components/contactUs/ContactUs.tsx
@@ -10,6 +10,7 @@ import { usePage } from '@inertiajs/react';
 import axios from 'axios';
 import PageLoading from '../ui/page-loading';
 import ContactInfo from './ContactInfo';
+import ContactMap from './ContactMap';
 import ContactSuccessSubmited from './ContactSuccessSubmited';
 
 export interface ContactFormData {
@@ -66,40 +67,45 @@ const ContactUs: React.FC = () => {
     }
 
     return (
-        <section
-            className={`${CLASS_NAME.bgDefault} p-[20px] md:p-[30px] lg:p-[40px] xl:p-[60px]`}
-            style={{
-                backgroundImage: 'url(assets/images/pattern-15.png)',
-                backgroundSize: 'cover',
-            }}
-        >
-            <div className="pt-[20px] md:pt-[30px]">
-                <div className="container mx-auto px-[12px] 2xl:max-w-[1320px]">
-                    <div className="grid grid-cols-1 items-center gap-[25px] lg:grid-cols-6 mx-auto">
-                        <div className="col-span-1 lg:col-span-2">
-                            <ContactInfo />
-                        </div>
+        <>
+            <section
+                className={`${CLASS_NAME.bgDefault} p-[20px] md:p-[30px] lg:p-[40px] xl:p-[60px]`}
+                style={{
+                    backgroundImage: 'url(assets/images/pattern-15.png)',
+                    backgroundSize: 'cover',
+                }}
+            >
+                <div className="pt-[20px] md:pt-[30px]">
+                    <div className="container mx-auto px-[12px] 2xl:max-w-[1320px]">
+                        <div className="grid grid-cols-1 items-center gap-[25px] lg:grid-cols-6 mx-auto">
+                            <div className="col-span-1 lg:col-span-2">
+                                <ContactInfo />
+                            </div>
 
-                        <div className="col-span-1 lg:col-span-4">
-                            <div className={`${CLASS_NAME.bgWhite} p-[10px] shadow-lg`}>
-                                <div className="mb-[25px] md:mb-[30px] md:max-w-[540px] lg:mb-[35px] lg:max-w-full xl:mb-[40px]">
-                                    <TitleBadgeOne title="Contact" />
-                                    <h2 className="!mb-0 text-2xl lg:text-3xl !leading-[1.2] -tracking-[.5px]  md:-tracking-[.6px] lg:-tracking-[.8px]  xl:-tracking-[1px]">
-                                        {t('CONTACT_US.TITLE', 'Comment pouvons-nous vous aider ? ')}
-                                    </h2>
-                                    <p className="mt-[10px] text-gray-600 dark:text-gray-400">
-                                        Remplissez le formulaire ci-dessous afin que nous puissions mieux vous connaître et répondre à vos besoins.
-                                    </p>
+                            <div className="col-span-1 lg:col-span-4">
+                                <div className={`${CLASS_NAME.bgWhite} p-[10px] shadow-lg`}>
+                                    <div className="mb-[25px] md:mb-[30px] md:max-w-[540px] lg:mb-[35px] lg:max-w-full xl:mb-[40px]">
+                                        <TitleBadgeOne title="Contact" />
+                                        <h2 className="!mb-0 text-2xl lg:text-3xl !leading-[1.2] -tracking-[.5px]  md:-tracking-[.6px] lg:-tracking-[.8px]  xl:-tracking-[1px]">
+                                            {t('CONTACT_US.TITLE', 'Comment pouvons-nous vous aider ? ')}
+                                        </h2>
+                                        <p className="mt-[10px] text-gray-600 dark:text-gray-400">
+                                            Remplissez le formulaire ci-dessous afin que nous puissions mieux vous connaître et répondre à vos
+                                            besoins.
+                                        </p>
+                                    </div>
+
+                                    <ContactForm handleSubmit={handleSubmit} />
                                 </div>
-
-                                <ContactForm handleSubmit={handleSubmit} />
                             </div>
                         </div>
-
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section>
+                <ContactMap />
+            </section>
+        </>
     );
 };
 
