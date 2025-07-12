d58ffb37cc24343c134cb4145a55d01e6056582d
eraste-dev <keraste38@gmail.com>
2025-07-12 13:32:14 +0000

    fix: correct image source fallback in ReferenceLogos component

--- resources/js/components/references/ReferenceLogos.tsx
+++ resources/js/components/references/ReferenceLogos.tsx
@@ -9,16 +9,13 @@ export default function ReferenceLogos({ references }: ReferenceLogosProps) {
         return null;
     }
 
+    //  ?? ref.media?.src
+
     return (
         <div className="py-8">
             <div className="container mx-auto flex flex-wrap justify-center gap-6">
                 {references.map((ref) => (
-                    <img
-                        key={ref.id}
-                        src={'https://placehold.jp/250x150.png' ?? ref.media?.src}
-                        alt={ref.text ?? ''}
-                        className="h-16 w-auto object-contain"
-                    />
+                    <img key={ref.id} src={'https://placehold.jp/250x150.png'} alt={ref?.text ?? ''} className="h-16 w-auto object-contain" />
                 ))}
             </div>
         </div>
