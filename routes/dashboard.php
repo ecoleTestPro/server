b78f637a424bd7922e9cbb635ecfebf098a6731f
eraste-dev <keraste38@gmail.com>
2025-07-12 04:28:58 +0000

    fix(faqs): correct restore route definition for FAQs

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -112,6 +112,6 @@ Route::middleware(['auth', 'verified'])->prefix('dashboard')->group(function () 
         Route::post('create',        [FaqController::class, 'store'])->name('dashboard.faqs.store');
         Route::put('update/{faq}',   [FaqController::class, 'update'])->name('dashboard.faqs.update');
         Route::delete('delete/{faq}', [FaqController::class, 'destroy'])->name('dashboard.faqs.delete');
-        Route::post('restore/{faq}', [FaqController::class, 'restore'])->name('dashboard.faqs.restore');
+        Route::post('restore/{faq}', [FaqController::class, 'restore'])->name('dashboard.faqs.restore'); 
     });
 });
