ba94eb0e57deb17fe86c14282cd4cef691b3af5b
eraste-dev <keraste38@gmail.com>
2025-07-12 06:16:08 +0000

    refactor(EnrollmentRate): improve null checks and simplify component structure

--- resources/js/components/charts/EnrollmentRate.tsx
+++ resources/js/components/charts/EnrollmentRate.tsx
@@ -1,27 +1,21 @@
 import { CLASS_NAME } from '@/data/styles/style.constant';
-import EnrollmentRateAreaChart from './EnrollmentRateAreaChart';
 import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
+import EnrollmentRateAreaChart from './EnrollmentRateAreaChart';
 
 export default function EnrollmentRate() {
     const { data } = usePage<SharedData>().props;
     const charts = data.chart_data;
 
-    if (!charts) return null;
+    if (!charts || !charts.enrollment_area || !charts.course_area || !charts.enrollment_area.categories || !charts.course_area.categories) {
+        return null;
+    }
 
     return (
         <section className={CLASS_NAME.section}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <EnrollmentRateAreaChart
-                    title="Inscriptions"
-                    series={charts.enrollment_area.series}
-                    categories={charts.enrollment_area.categories}
-                />
-                <EnrollmentRateAreaChart
-                    title="Formations"
-                    series={charts.course_area.series}
-                    categories={charts.course_area.categories}
-                />
+                <EnrollmentRateAreaChart title="Inscriptions" series={charts.enrollment_area.series} categories={charts.enrollment_area.categories} />
+                <EnrollmentRateAreaChart title="Formations" series={charts.course_area.series} categories={charts.course_area.categories} />
             </div>
         </section>
     );
