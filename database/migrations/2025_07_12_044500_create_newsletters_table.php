cf2a126fe5fb1699cdadc1bf74029cca209293c7
eraste-dev <keraste38@gmail.com>
2025-07-12 09:06:39 +0000

    feat: add soft deletes to newsletters table

--- database/migrations/2025_07_12_044500_create_newsletters_table.php
+++ database/migrations/2025_07_12_044500_create_newsletters_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
