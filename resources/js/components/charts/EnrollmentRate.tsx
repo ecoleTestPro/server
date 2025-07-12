cacaa0325f91e9b9f27ef772707449af13f44dc4
eraste-dev <keraste38@gmail.com>
2025-07-12 08:34:23 +0000

    feat(dashboard): update layout and improve grid structure for DashboardWidget and EnrollmentRate components

--- resources/js/components/charts/EnrollmentRate.tsx
+++ resources/js/components/charts/EnrollmentRate.tsx
@@ -15,7 +15,7 @@ export default function EnrollmentRate() {
         <section className={CLASS_NAME.section}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <EnrollmentRateAreaChart title="Inscriptions" series={charts.enrollment_area.series} categories={charts.enrollment_area.categories} />
-                <EnrollmentRateAreaChart title="Formations" series={charts.course_area.series} categories={charts.course_area.categories} />
+                <EnrollmentRateAreaChart title="Graphe de formation" series={charts.course_area.series} categories={charts.course_area.categories} />
             </div>
         </section>
     );
--- resources/js/pages/dashboard/dashboard.home.tsx
+++ resources/js/pages/dashboard/dashboard.home.tsx
@@ -12,21 +12,11 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard() {
-    // const { t } = useTranslation();
-
-    // const { data } = usePage<SharedData>().props;
-
-    // const [showConfirm, setShowConfirm] = useState(false);
-    // const [isDeleting, setIsDeleting] = useState(false);
-
-    // const [open, setOpen] = useState(false);
-    // const [selected, setCategory] = useState<ICourseForm | undefined>(undefined);
-
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="">
+                <div className="grid grid-cols-1 gap-4 ">
                     <DashboardWidget />
                     <EnrollmentRate />
                 </div>
