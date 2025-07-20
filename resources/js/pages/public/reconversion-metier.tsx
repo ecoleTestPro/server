c492af1da53ad3ff261308fdb57d4e15e288d0bd
eraste-dev <keraste38@gmail.com>
2025-07-20 19:18:48 +0000

    refactor: reorganize layout structure and improve content presentation on reconversion page

--- resources/js/pages/public/reconversion-metier.tsx
+++ resources/js/pages/public/reconversion-metier.tsx
@@ -1,6 +1,6 @@
 import Hero, { IHeroBreadcrumbItems } from '@/components/hero/hearo';
-import BtnSecondary from '@/components/ui/button/btn-secondary';
 import StatsBlock from '@/components/reconversion/stats-block';
+import BtnSecondary from '@/components/ui/button/btn-secondary';
 import DefaultLayout from '@/layouts/public/front.layout';
 import { SharedData } from '@/types';
 import { ROUTE_MAP } from '@/utils/route.util';
@@ -25,25 +25,33 @@ export default function ReconversionMetierPage() {
                     breadcrumbItems={breadcrumbItems}
                     gradient="style-2"
                 />
-                <StatsBlock />
-                <div className="container mx-auto px-4 py-8 space-y-4">
-                    <p>
-                        TestPro accompagne les centres de formation, écoles et ESN souhaitant délivrer des formations de reconversion au métier «
-                        Testeur Logiciel » (POEI, POEC, Programme Région, Rescaling des ressources …).
-                    </p>
-                    <p>
-                        Nos programmes sont animés par des formateurs experts certifiés et reconnus dans leur domaine, s’appuyant sur notre kit
-                        méthodologique de conduite de projet « QUP » développé à partir de retours d’expériences et des standards internationaux -
-                        ISTQB, IREB, IQBBA, TMAP, TMMi, CMMi, ISO 25000.
-                    </p>
-                    <ul className="list-disc pl-5 space-y-1">
-                        <li>Testeur Manuel</li>
-                        <li>Testeur Automatisation</li>
-                        <li>Testeur Full Stack</li>
-                    </ul>
-                    <BtnSecondary label="Contactez-nous" href={ROUTE_MAP.public.contact.link} />
-                    <img src="/assets/images/pexels-shvetsa-3727464.jpg" alt="illustration" className="mx-auto" />
+                <div className="container mx-auto py-[50px] px-[10px]">
+                    <div className="grid grid-cols-1 lg:grid-cols-5 gap-4">
+                        <div className="col-span-1 lg:col-span-3">
+                            <p>
+                                TestPro accompagne les centres de formation, écoles et ESN souhaitant délivrer des formations de reconversion au
+                                métier « Testeur Logiciel » (POEI, POEC, Programme Région, Rescaling des ressources …).
+                            </p>
+                            <p>
+                                Nos programmes sont animés par des formateurs experts certifiés et reconnus dans leur domaine, s’appuyant sur notre
+                                kit méthodologique de conduite de projet « QUP » développé à partir de retours d’expériences et des standards
+                                internationaux - ISTQB, IREB, IQBBA, TMAP, TMMi, CMMi, ISO 25000.
+                            </p>
+                            <ul className="list-disc pl-5 space-y-1">
+                                <li>Testeur Manuel</li>
+                                <li>Testeur Automatisation</li>
+                                <li>Testeur Full Stack</li>
+                            </ul>
+                            <BtnSecondary label="Contactez-nous" href={ROUTE_MAP.public.contact.link} />
+                        </div>
+                        <div className='col-span-1 lg:col-span-2' >
+                            <img src="/assets/images/pexels-shvetsa-3727464.jpg" alt="illustration" className="mx-auto w-full h-auto object-fill" />
+                        </div>
+                    </div>
                 </div>
+
+                <StatsBlock />
+                <div className="container mx-auto px-4 py-8 space-y-4"></div>
             </div>
         </DefaultLayout>
     );
