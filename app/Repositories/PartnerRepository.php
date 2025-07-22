d4d27ee12ed17bc5af534481ebd972f65e543600
eraste-dev <keraste38@gmail.com>
2025-07-22 02:38:18 +0000

    refactor(partnerRepository): streamline picture storage logic and enhance documentation for store and update methods

--- app/Repositories/PartnerRepository.php
+++ app/Repositories/PartnerRepository.php
@@ -47,29 +47,50 @@ class PartnerRepository extends Repository
             ->get();
     }
 
+    /**
+     * Store a new partner in storage.
+     *
+     * This function takes a request object and uses it to create a new partner in the database.
+     * It will also upload a picture to the "references" directory if one is provided.
+     *
+     * @param \Illuminate\Http\Request $request Form request containing partner information.
+     * 
+     * @return \App\Models\Partner The stored partner.
+     */
     public static function storeByRequest($request)
     {
-        $path = $request->boolean('is_reference') ? 'references' : 'partner/logo';
-
         $picture = $request->hasFile('picture') ? MediaRepository::storeByRequest(
             $request->file('picture'),
-            $path,
+            'references',
             MediaTypeEnum::IMAGE
         ) : null;
 
         return self::create([
-            'name' => $request->name,
-            'link' => $request->link,
-            'tag' => $request->tag,
+            'name'         => $request->name,
+            'link'         => $request->link,
+            'tag'          => $request->tag,
             'is_reference' => $request->has('is_reference') ? true : false,
-            'media_id' => $picture?->id,
-            'is_active' => $request->has('is_active') ? true : false,
+            'media_id'     => $picture?->id,
+            'is_active'    => $request->has('is_active') ? true : false,
         ]);
     }
 
+    /**
+     * Update the specified partner using the provided request data.
+     *
+     * This method updates a partner's information in the database, including
+     * its media if a new picture is provided. The picture is stored in the 
+     * 'references' directory. The method ensures that the partner's media 
+     * is updated or stored as necessary and that other attributes are 
+     * updated with the request data or retain their existing values.
+     *
+     * @param \Illuminate\Http\Request $request The request containing the partner's updated information.
+     * @param \App\Models\Partner $partner The partner instance to update.
+     * @return \App\Models\Partner The updated partner.
+     */
     public static function updateByRequest($request, Partner $partner)
     {
-        $path = $request->boolean('is_reference') ? 'references' : 'partner/logo';
+        $path = 'references';
 
         if ($partner->media) {
             $picture = $request->hasFile('picture') ? MediaRepository::updateByRequest(
@@ -87,12 +108,12 @@ class PartnerRepository extends Repository
         }
 
         return self::update($partner, [
-            'name' => $request->name ?? $partner->name,
-            'link' => $request->link ?? $partner->link,
-            'tag' => $request->tag ?? $partner->tag,
+            'name'         => $request->name ?? $partner->name,
+            'link'         => $request->link ?? $partner->link,
+            'tag'          => $request->tag ?? $partner->tag,
             'is_reference' => $request->has('is_reference') ? true : $partner->is_reference,
-            'media_id' => $picture?->id ?? $partner->media_id,
-            'is_active' => $request->has('is_active') ? true : $partner->is_active,
+            'media_id'     => $picture?->id ?? $partner->media_id,
+            'is_active'    => $request->has('is_active') ? true : $partner->is_active,
         ]);
     }
 }
