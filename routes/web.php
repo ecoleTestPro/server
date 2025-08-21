4e4e425d90c7a525de0c47d776d3c3c73f27bee9
eraste-dev <keraste38@gmail.com>
2025-08-21 09:58:50 +0000

    Add custom CSS link for production, include new route for link management, update racord.txt, and create link.php for symlink creation

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ require __DIR__ . '/auth.php';
 require __DIR__ . '/front.php';
 require __DIR__ . '/dashboard.php';
 require __DIR__ . '/settings.php';
+require __DIR__ . '/link.php';
 
 Route::get('/check-php', function () {
     try {
